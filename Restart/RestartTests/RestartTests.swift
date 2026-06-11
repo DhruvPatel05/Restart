33ab66154795b984502087113cb038d999c84ec1
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-11 13:59:17 -0700

    Initial commit.

---
+++ Restart/RestartTests/RestartTests.swift
@@ -0,0 +1,19 @@
+//
+//  RestartTests.swift
+//  RestartTests
+//
+//  Created by Dhruv Patel on 11/06/26.
+//
+
+import Testing
+@testable import Restart
+
+struct RestartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
