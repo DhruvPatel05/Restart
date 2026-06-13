b55bdb24881dd65413adeaaeae861fe6e787066c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-13 15:18:36 -0700

    onboarding screen added.

--- Restart/Restart/Screens/OnBoardingView.swift
+++ Restart/Restart/Screens/OnBoardingView.swift
@@ -13,16 +13,22 @@ struct OnBoardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
 
     var body: some View {
-        VStack (spacing: 20){
-            Text("Onboarding")
-                .font(.largeTitle)
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all,edges:.all)
             
-            Button(action:{
-                isOnboardingViewActive = false
-            }) {
-                Text("Start")
-            }
-        }  //: VStack
+            VStack (spacing: 20){
+                // MARK: - Header
+                
+                
+                // MARK: - Center
+                
+                
+                // MARK: - Footer
+                
+                
+            }//: VStack
+        }//: ZStack
     }
 }
 struct OnBoardingView_Previews: PreviewProvider {
