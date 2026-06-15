6d1964638f92d94829d4f77fc62bb42602d78b48
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-14 19:37:43 -0700

    Capsule Design added.

--- Restart/Restart/Screens/OnBoardingView.swift
+++ Restart/Restart/Screens/OnBoardingView.swift
@@ -63,8 +63,35 @@ It's not how much we give but how much love we put into giving.
 
                         .padding(8)
                   // 2.CALL-TO-ACTION (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
                   // 3.CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack() {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width:80)
+                        Spacer()
+                    }
                   // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80,height: 80,alignment: .center)
+                        Spacer()
+                    }//: HSTACK
                 }//: FOOTER
                 .frame(height: 80,alignment: .center)
                 .padding()
