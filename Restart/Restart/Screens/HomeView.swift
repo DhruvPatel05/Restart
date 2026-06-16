be24930ac5b024ba1566d6c5dd481b3aa7ab6c0c
Dhruv Patel <dhruvpatel913@gmail.com>
2026-06-16 13:20:21 -0700

    HomeView animation added

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -11,7 +11,8 @@ struct HomeView: View {
     
     // MARK: - Preoperty
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = false
-
+    @State private var isAnimating: Bool = false
+    
     // MARK: - Body
     var body: some View {
         VStack(spacing:20) {
@@ -23,6 +24,10 @@ struct HomeView: View {
                     .resizable()
                     .scaledToFit()
                     .padding()
+                    .offset(y: isAnimating ? 35 : -35)
+                    .animation(Animation.easeInOut(duration: 4)
+                        .repeatForever(), value: isAnimating
+                    )
             }
             
             // MARK: - CENTER
@@ -39,7 +44,9 @@ struct HomeView: View {
             Spacer()
             
             Button(action:{
-                isOnboardingViewActive = true
+                withAnimation {
+                    isOnboardingViewActive = true
+                }
             }) {
                 Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
                     .imageScale(.large)
@@ -52,6 +59,11 @@ struct HomeView: View {
             .buttonBorderShape(.capsule)
             .controlSize(.large)
         }//: VSTACK
+        .onAppear(perform: {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3, execute: {
+                isAnimating = true
+            })
+        })
     }
 }
 
